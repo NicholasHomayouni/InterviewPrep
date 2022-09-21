ac46dd59c41ee9ccddcdf152c8fc254e43451c76
Nicholas Homayouni <>
2022-09-21 17:39:48 -0400

    added Local time variable

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,17 @@
-
-
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 class Result {
-    public static void timeConversion(String s) {
-
+    public static String timeConversion(String s) {
+        String result = LocalTime.parse(s, DateTimeFormatter.ofPattern("hh mm ss a", Locale.US))
+                .format(DateTimeFormatter.ofPattern("HH mm ss"));
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
 
+        String s = "12:01:00AM";
     }
 }
