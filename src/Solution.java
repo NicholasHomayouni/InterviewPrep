13520110b40125076e8d161ec47fdb85523d3203
Nicholas Homayouni <>
2022-09-22 14:34:46 -0400

    started lonelyInteger Problem

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,13 @@
-//class Result {
-//    public static String timeConversion() {
-//        Date dt = new Date();   // making a new date object                         // lowercase k equates to hour(1-24)
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("kk:mm:ss a");   // making a new dateFormat object with pattern
-//        return dateFormat.format(dt);   // returns the time in a 24-hour format
-//    }
-//}
+import java.util.List;
 
-class Demo {
-    public static String convDemo(String str) {                 // parse first two digits
-        int hour = Integer.parseInt(str.substring(0, 2)) % 12;  // beg. index and ending index. if # = 12, set it to 0
-        if (str.endsWith("PM")) {   // if input ends with PM
-            hour += 12;             // add 12
-        }                   // %02d = format integer w/ 2 digits, left-padding w/ zeroes
-        return String.format("%02d", hour) + str.substring(2, 8);     // replace first two digits with new #, remove AM/PM suffix
+class Result {
+    public static int lonelyInteger(List<Integer> a) {
+
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        //System.out.println(Result.timeConversion());    // testing
-        System.out.println(Demo.convDemo("12:30:45PM"));    // testing
+
     }
 }
