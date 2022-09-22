f6514a3536384a680ba0ddd3e4f17420aa5bff15
Nicholas Homayouni <>
2022-09-22 11:56:00 -0400

    easier strategy for converting

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,16 @@
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class Result {
-    public static String timeConversion(String s) {
-        String result = LocalTime.parse(s, DateTimeFormatter.ofPattern("hh mm ss a", Locale.US))
-                .format(DateTimeFormatter.ofPattern("HH mm ss"));
+    public static String timeConversion() {
+        Date dt = new Date();   // making a new date object                         // lowercase k equates to hour(1-24)
+        SimpleDateFormat dateFormat = new SimpleDateFormat("kk:mm:ss a");   // making a new dateFormat object with pattern
+        return dateFormat.format(dt);   // returns the time in a 24-hour format
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-
-        String s = "12:01:00AM";
+        System.out.println(Result.timeConversion());    // testing
     }
 }
