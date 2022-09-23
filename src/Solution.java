f7ea20d172f8d5c51206f98df631a08fccc49437
Nicholas Homayouni <>
2022-09-23 09:45:43 -0400

    use hashSet for finding unique value

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 class Result {
     public static int lonelyInteger(List<Integer> a) {
+        HashSet<Integer> hset = new HashSet<Integer>(a);
 
+        //System.out.println(hset);
+
+        return ;
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-
+        Result.lonelyInteger();
     }
 }
