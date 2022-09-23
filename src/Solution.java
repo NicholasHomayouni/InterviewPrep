e4922c028cb65f034451dfa2aeee3bb41100c9be
Nicholas Homayouni <>
2022-09-23 17:11:41 -0400

    works when function is changed to return boolean

--- src/Solution.java
+++ src/Solution.java
@@ -2,25 +2,31 @@ import java.util.Arrays;
 import java.lang.*;
 
 class Demo {
-    public static int Solve(String[] A) {
+    public static boolean Solve(String[] A) {
         String str = Arrays.toString(A);
-        if (str.length() > 128) return 0;       // base case if string length exceeds # of unique characters in alphabet
+        if (str.length() > 26) return false;       // base case if string length exceeds # of unique characters in alphabet
 
         boolean[] char_set = new boolean[128];
+
         for (int i = 0; i < A.length; i++) {
             int val = Arrays.toString(A).charAt(i);     // return each character of string in array
             if (char_set[val]) {    // if we already found this character in the string, return false
-                return 0;
+                return false;
             }
             char_set[val] = true;   // returns true if we haven't found this character in the string
         }
-        return 1;   // return true if unique
+        return true;   // return true if unique
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        String[] array = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
-        Demo.Solve(array);
+        String[] str = {"scaler", "interviewbit"};
+        Demo.Solve(str);
+
+        if (Demo.Solve(str))
+            System.out.println(1);
+        else
+            System.out.println(0);
     }
 }
