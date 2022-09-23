609121dc08bcbaf5e080a564e15bb42add98af86
Nicholas Homayouni <>
2022-09-23 09:47:22 -0400

    first commit in CtCI

--- src/Solution.java
+++ src/Solution.java
@@ -4,16 +4,12 @@ import java.util.List;
 
 class Result {
     public static int lonelyInteger(List<Integer> a) {
-        HashSet<Integer> hset = new HashSet<Integer>(a);
 
-        //System.out.println(hset);
-
-        return ;
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        Result.lonelyInteger();
+
     }
 }
