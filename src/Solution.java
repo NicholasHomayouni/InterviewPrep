a1081fde7682a18a8c6dd1e6099d1afb0fa85c36
Nicholas Homayouni <>
2022-09-23 14:12:46 -0400

    first commit in CtCI

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,16 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
-class Result {
-    public static int lonelyInteger(List<Integer> a) {
-
+class Demo {
+    public static void printPairs(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++){
+                System.out.print(array[i] + "," + array[j]);
+            }
+        }
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-
+        int[] array = {1,2,3,4,5,6};
+        Demo.printPairs(array);
     }
 }
