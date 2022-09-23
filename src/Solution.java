ff4cbf48fc20817983050866e34bdabe2653897c
Nicholas Homayouni <>
2022-09-23 16:21:55 -0400

    pangram problem worked on

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,24 @@
+import java.util.Arrays;
+import java.lang.*;
+
 class Demo {
-    public static void printPairs(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++){
-                System.out.print(array[i] + "," + array[j]);
+    public static int Solve(String[] A) {
+        boolean[] char_set = new boolean[128];
+
+        for (int i = 0; i < A.length; i++) {
+            char val = Arrays.toString(A).charAt(i);
+            if (char_set[val]) {    // if we already found this character in the string, return false
+                return 0;
             }
+            char_set[val] = true;   // returns true if we havent found this character in the string
         }
+        return 1;   // return true if unique
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6};
-        Demo.printPairs(array);
+        String[] array = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
+        Demo.Solve(array);
     }
 }
