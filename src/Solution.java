db6bf525d2635ced53a5e32585383bd33538d1e4
Nicholas Homayouni <>
2022-09-26 15:39:07 -0400

    pangram check overworked on

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,34 @@
-import java.util.Arrays;
-import java.lang.*;
+import java.util.ArrayList;
 
 class Demo {
-    public static boolean Solve(String[] A) {
-        String str = Arrays.toString(A);
-        if (str.length() > 26) return false;       // base case if string length exceeds # of unique characters in alphabet
-
-        boolean[] char_set = new boolean[128];
-
-        for (int i = 0; i < A.length; i++) {
-            int val = Arrays.toString(A).charAt(i);     // return each character of string in array
-            if (char_set[val]) {    // if we already found this character in the string, return false
-                return false;
+    public int Solve(ArrayList<String> A) {
+        //if (A.size() > 26) return 0;
+        boolean[] boolArr = new boolean[A.size()];
+        int abcIndex = 0;
+        for (int i = 0; i < A.size(); i++) {
+            String str = A.get(i);  // get each index and store it as a string
+            if('a' <= str.charAt(i) & 'z' >= str.charAt(i)) {
+                abcIndex = str.charAt(i) - 'a';
+                boolArr[abcIndex] = true;
+            //    return 0;
             }
-            char_set[val] = true;   // returns true if we haven't found this character in the string
         }
-        return true;   // return true if unique
+        for (boolean index : boolArr) {
+            if (!index) {
+                return 0;
+            }
+        } return 1;
+//        if ( <= 26)
+//            return 1;
+//        else
+//            return 0;
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        String[] str = {"scaler", "interviewbit"};
-        Demo.Solve(str);
-
-        if (Demo.Solve(str))
-            System.out.println(1);
-        else
-            System.out.println(0);
+        ArrayList<String> s = new ArrayList<String>();
+        s.add("interviewbit");
+        s.add("pangram");
     }
 }
