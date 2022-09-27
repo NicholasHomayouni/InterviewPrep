69d9f801a900047f4694f2380a4bf184fc58c75b
Nicholas Homayouni <>
2022-09-27 15:13:01 -0400

    lonelyInteger Challenge Complete

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,24 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-
-class Result {
+   class Result {
     public static int lonelyInteger(List<Integer> a) {
-        HashSet<Integer> hset = new HashSet<Integer>(a);
-
-        //System.out.println(hset);
-
-        return ;
+        int res = 0;
+        for (int i = 0; i < a.size(); i++)
+            res = res ^ a.get(i);
+        return res;
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        Result.lonelyInteger();
+        List<Integer> lst = new ArrayList<>();
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+        lst.add(4);
+        lst.add(3);
+        lst.add(2);
+        lst.add(1);
+        Result.lonelyInteger(lst);
     }
 }
