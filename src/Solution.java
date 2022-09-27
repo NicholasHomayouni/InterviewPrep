d459905844cd5b70043e7d928f1e056f66a5ec8f
Nicholas Homayouni <>
2022-09-27 15:23:55 -0400

    diagonalFlip Challenge

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,12 @@
-import java.util.ArrayList;
 import java.util.List;
-   class Result {
-    public static int lonelyInteger(List<Integer> a) {
-        int res = 0;
-        for (int i = 0; i < a.size(); i++)
-            res = res ^ a.get(i);
-        return res;
+class Result {
+    public static int[] diagonalFlip(int[][] arr) {
+
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        List<Integer> lst = new ArrayList<>();
-        lst.add(1);
-        lst.add(2);
-        lst.add(3);
-        lst.add(4);
-        lst.add(3);
-        lst.add(2);
-        lst.add(1);
-        Result.lonelyInteger(lst);
+
     }
 }
