b55ad9888a2a377416827ab5d7b7a88ede21b4f0
Nicholas Homayouni <>
2022-09-26 21:17:57 -0400

    Correct Pangram Solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,25 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 class Demo {
-    public static void Solve() {
-
+    public static int Solve(String[] A) {
+        Set<Character> char_set = new HashSet<>();
+        for (int i = 0; i <A.length; i++) {
+            String str = A[i];
+            for (int j = 0; j < str.length(); j++) {
+                char_set.add(str.charAt(j));
+            }
+        }
+        if (char_set.size() == 26)
+            return 1;
+        else
+            return 0;
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-
-        Demo.Solve();
+        String[] s = {"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
+        Demo.Solve(s);
     }
 }
