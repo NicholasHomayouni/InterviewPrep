62c4b8f68d0a624f9a6dc47f1e692a31a988ccb4
Nicholas Homayouni <>
2022-09-29 17:09:54 -0400

    valid password challenge complete

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,24 @@
 class Result {
     static int solve(String A) {
-        char[] charArr = A.toCharArray();
-        boolean val = true;
+        //char[] charArr = A.toCharArray();
+        boolean n1 = false, n2 = false, n3 = false, n4 = false, n5 = false;
         for (int i = 0; i < A.length(); i++) {
-            if(!Character.isDigit(A.charAt(i))) {
-                val = false;
-            } else if (!Character.isUpperCase(A.charAt(i))) {
-                val = false;
-            } else if (!Character.isLowerCase(A.charAt(i))) {
-                val = false;
+            if(Character.isDigit(A.charAt(i))) {
+                n1 = true;
+            } else if (Character.isUpperCase(A.charAt(i))) {
+                n2 = true;
+            } else if (Character.isLowerCase(A.charAt(i))) {
+                n3 = true;
             } else if (A.charAt(i) == '!' || A.charAt(i) == '@'
                     || A.charAt(i) == '#' || A.charAt(i) == '$'
                     || A.charAt(i) == '%' || A.charAt(i) == '&' || A.charAt(i) == '*') {
-                val = true;
+                n4 = true;
             }
         }
-        if (A.length() < 8 || A.length() > 15) {
-            return 0;
+        if (A.length() >= 8 && A.length() <= 15) {
+            n5 = true;
         }
-        if (val)
+        if (n1 && n2 && n3 && n4 && n5)
             return 1;
         else
             return 0;
