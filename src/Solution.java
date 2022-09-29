d7b0b81ff9b0c6344e05e5f52a1a1916bf498998
Nicholas Homayouni <>
2022-09-29 16:09:49 -0400

    bad first draft

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,5 @@
 import java.util.HashSet;
 import java.util.Set;
-
 /*
 Password MUST:
 - have at least one digit (0-9)
@@ -8,28 +7,8 @@ Password MUST:
 - have at least one lowercase letter (a-z) and one uppercase letter (A-Z)
 - have at least one special character (@,#,%,$,!,*,&)
  */
-
-/*
-Algorithm pseudocode:
-
-// iterate through char_Arr[] and map each index letter to its hashcode
-- Set<Character> char_set = new HashSet<>()
-- for (int i = 0; i < char_Arr.length; i++)
-    `
--       if (char_arr[i].contains(SPECIAL_CHARACTERS))
-            && char_Arr[i].contains(0...9)
-            && char_Arr.length >= 8 && char_Arr.length <= 15
-                    && char_Arr[i].contains(UPPERCASE_LETTERS >= 1)
-                    && char_Arr[i].contains(lowercase_letters >= 1)
-            return 1;
-        else
-            return 0;
-
- */
-
 class Result {
     static int solve(String A) {
-        Set<Character> char_set = new HashSet<>();
         for (int i = 0; i < A.length(); i++) {
             char c = A.charAt(i);
         }
