ca7dd698686d68759c873cd2d90b4969e82ba92a
Nicholas Homayouni <>
2022-09-29 12:40:13 -0400

    starting template for problem

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,11 @@
 class Result {
-    // function to flip matrix along diagonals
-    public static int[][] diagonalFlip(int[][] A) {
-        int n = A.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                // do diagonal flipping alg...
-                int temp = A[i][j];
-                A[i][j] = A[j][i];
-                A[j][i] = temp;
-            }
-        }
-        return A;
+    static int solve(String A) {
+
     }
-
-    // function print and format the matrix
-    static void show(int[][] A) {
-        int n = A.length;
-        int m = A[0].length;
-
-        int i, j;
-        for (i = 0; i < m; i++) {
-            for (j = 0; j < n; j++) {
-                System.out.print(A[i][j] + "  ");
-            }
-            System.out.println();
-        }
-    }
-
 }
 
 public class Solution {
     public static void main(String[] args) {
-        int[][] arr = { {1,2},
-                        {3,4}};
-        Result.diagonalFlip(arr);
-        Result.show(arr);
+
     }
 }
