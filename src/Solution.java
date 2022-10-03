f143ec7c8fb7dee87e4f00d0e9accba1aa1cca1a
Nicholas Homayouni <>
2022-10-03 10:29:32 -0400

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,15 @@
 import java.util.ArrayList;
-
-// use n x n boolean matrix
-// true return value at matrix[i][j] indicates an edge from node i to node j
-// there is an undirected edge between B[i][0] and B[i][1]
-// find the number of connected nodes (return A)
+/*
+Given a string A and a dictionary of words B, determine if A can be segmented into a
+space-separated sequence of one or more dictionary words.
+ */
  class Result {
-    public int solve(int A, int[][] B) {
-        ArrayList<Integer>[] graph = new ArrayList[A + 1];
-        for(int[] e : B){
-            if(graph[e[0]] == null){
-                graph[e[0]] = new ArrayList<>();
-            }
-            if(graph[e[1]] == null){
-                graph[e[1]] = new ArrayList<>();
-            }
-            graph[e[0]].add(e[1]);
-            graph[e[1]].add(e[0]);
-        }
-        visited = new boolean[A + 1];
-        int count = 0;
-        for(int i = 1; i <= A; ++i){
-            if(!visited[i]){
-                visited[i] = true;
-                ++count;
-                dfs(graph, i);
-            }
-        }
-        return count;
+    public int wordBreak(String A, ArrayList<String> B) {
 
     }
+ }
 
-    boolean[] visited;
-
-    void dfs(ArrayList<Integer>[] graph, int cur){
-        if(graph[cur] != null){
-            for(int e : graph[cur]){
-                if(!visited[e]){
-                    visited[e] = true;
-                    dfs(graph, e);
-                }
-            }
-        }
-    }
-}
-
-public class Solution {
+ public class Result {
     public static void main(String[] args) {
 
     }
--- src/syntaxFun.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    print(sum(4,2))
-}
-
-fun sum(a: Int, b: Int): Int {
-    return a + b
-}
