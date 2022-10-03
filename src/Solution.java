4116908e6dac68318dfb0c1d2d0d894235fae59d
Nicholas Homayouni <>
2022-10-03 10:21:00 -0400

    solution code for challenge

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,51 @@
-import java.util.Stack;
+import java.util.ArrayList;
 
 // use n x n boolean matrix
 // true return value at matrix[i][j] indicates an edge from node i to node j
 // there is an undirected edge between B[i][0] and B[i][1]
 // find the number of connected nodes (return A)
-class Result {
+ class Result {
     public int solve(int A, int[][] B) {
-        // connected components part
-        boolean[] visited =  new boolean[A];
-        for (int i = 0; i < B.length; i++) {
-            if (!visited[i]) {
-                // run DFS:
-                dFS(i, visited, B);
+        ArrayList<Integer>[] graph = new ArrayList[A + 1];
+        for(int[] e : B){
+            if(graph[e[0]] == null){
+                graph[e[0]] = new ArrayList<>();
+            }
+            if(graph[e[1]] == null){
+                graph[e[1]] = new ArrayList<>();
+            }
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
+        visited = new boolean[A + 1];
+        int count = 0;
+        for(int i = 1; i <= A; ++i){
+            if(!visited[i]){
+                visited[i] = true;
+                ++count;
+                dfs(graph, i);
             }
         }
-
-        // DFS util
-        visited[A] = true;
+        return count;
 
     }
 
-    public void dFS(int v, boolean[] visited, int[][] B) {
-        visited[v] = true;
-        for (int i = 0; i < B[A].length; i++) {
+    boolean[] visited;
 
+    void dfs(ArrayList<Integer>[] graph, int cur){
+        if(graph[cur] != null){
+            for(int e : graph[cur]){
+                if(!visited[e]){
+                    visited[e] = true;
+                    dfs(graph, e);
+                }
+            }
         }
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        int[][] arr = { {1,2},
-                        {2,3}};
-        Result.solve(4, arr);
+
     }
 }
