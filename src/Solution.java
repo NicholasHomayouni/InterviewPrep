99a9562aad79defe1b512d88a1fcb0b764791354
Nicholas Homayouni <>
2022-10-17 10:58:41 -0400

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-   class Result {
-    public static int lonelyInteger(List<Integer> a) {
-        int res = 0;
-        for (int i = 0; i < a.size(); i++)
-            res = res ^ a.get(i);
-        return res;
+import java.util.HashSet;
+
+class Demo {
+    public int[] set_intersection(HashSet<Integer> set1, HashSet<Integer> set2) {
+
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<Integer>(); // CONVERT BOTH ARRAYS INTO SETS
+        for (Integer n : nums1) set1.add(n);
+
+        HashSet<Integer> set2 = new HashSet<Integer>(); // CONVERT BOTH ARRAYS INTO SETS
+        for (Integer n : nums2) set2.add(n);
+
+        if (set1.size() < set2.size()) return
     }
 }
 
 public class Solution {
     public static void main(String[] args) {
-        List<Integer> lst = new ArrayList<>();
-        lst.add(1);
-        lst.add(2);
-        lst.add(3);
-        lst.add(4);
-        lst.add(3);
-        lst.add(2);
-        lst.add(1);
-        Result.lonelyInteger(lst);
+
     }
 }
