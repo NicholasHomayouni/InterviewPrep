efc66cbd4cb0b491f9deb8f08300f38763a14c58
Nicholas Homayouni <>
2022-10-25 14:19:38 -0400

    uniqueChars

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,26 @@
 import java.util.HashMap;
-import java.util.Map;
 
-// "static void main" must be defined in a public class.
+class Demo {
+    public int firstUniqChar(String s) {
+// HashMap <characters, #times occurred> if unique, value should be 1
+        HashMap<Character, Integer> map = new HashMap<>();
+// go through string and save in hashmap # of times each char appears
+        for (int i=0; i<s.length(); ++i) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1 );
+        }
+
+// find the index using the hashmap as a reference to check chars for uniqueness
+        for (int i=0; i<s.length(); ++i) {
+            if (map.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+}
+
 public class Solution {
     public static void main(String[] args) {
-        // 1. initialize a hash map
-        HashMap<Integer, Integer> hashmap = new HashMap<>();
-        // 2. insert a new (key, value) pair
-        hashmap.putIfAbsent(0, 0);
-        hashmap.putIfAbsent(2, 3);
-        // 3. insert a new (key, value) pair or update the value of existed key
-        hashmap.put(1, 1);
-        hashmap.put(1, 2);
-        // 4. get the value of specific key
-        System.out.println("The value of key 1 is: " + hashmap.get(1));
-        // 5. delete a key
-        hashmap.remove(2);
-        // 6. check if a key is in the hash map
-        if (!hashmap.containsKey(2)) {
-            System.out.println("Key 2 is not in the hash map.");
-        }
-        // 7. get the size of the hash map
-        System.out.println("The size of hash map is: " + hashmap.size());
-        // 8. iterate the hash map
-        for (Map.Entry<Integer, Integer> entry : hashmap.entrySet()) {
-            System.out.print("(" + entry.getKey() + "," + entry.getValue() + ") ");
-        }
-        System.out.println("are in the hash map.");
-        // 9. clear the hash map
-        hashmap.clear();
-        // 10. check if the hash map is empty
-        if (hashmap.isEmpty()) {
-            System.out.println("hash map is empty now!");
-        }
+
     }
 }
