00fe85a56f768565cf3c6c25e46c0ea387d4888b
Nicholas Homayouni <>
2022-10-25 14:23:43 -0400

    new commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,16 @@
 import java.util.HashMap;
 
 class Demo {
-    public int firstUniqChar(String s) {
-// HashMap <characters, #times occurred> if unique, value should be 1
+    public static int firstUniqChar(String s) {
+        // HashMap <characters, #times occurred> if unique, value should be 1
         HashMap<Character, Integer> map = new HashMap<>();
-// go through string and save in hashmap # of times each char appears
+        // go through string and save in hashmap # of times each char appears
         for (int i=0; i<s.length(); ++i) {
             char c = s.charAt(i);
             map.put(c, map.getOrDefault(c, 0) + 1 );
         }
 
-// find the index using the hashmap as a reference to check chars for uniqueness
+        // find the index using the hashmap as a reference to check chars for uniqueness
         for (int i=0; i<s.length(); ++i) {
             if (map.get(s.charAt(i)) == 1)
                 return i;
@@ -21,6 +21,7 @@ class Demo {
 
 public class Solution {
     public static void main(String[] args) {
-
+        String s = "LOVELEETCODE";
+        Demo.firstUniqChar(s);
     }
 }
